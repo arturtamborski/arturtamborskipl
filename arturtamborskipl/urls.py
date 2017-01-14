d81a2b0f5a255979444a18903ffb2f5d0d76b561
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-14 02:21:51 +0100

    add sitemap to urls

--- arturtamborskipl/urls.py
+++ arturtamborskipl/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.sitemaps.views import sitemap
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -5,4 +6,6 @@ urlpatterns = [
     url(r'^', include('blog.urls')),
     url(r'^blog', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
