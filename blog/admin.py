e45518f4a141ca59c64c0f6efe22e841669fbff1
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-14 01:11:25 +0100

    add models admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models as blog 
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    ordering = ('-date',)
+    list_filter = ('id', 'date',)
+    list_display = ('id', 'title', 'date', 'category',)
+    search_fields = ('id', 'title', 'date', 'category',)
+    date_hierarchy = 'date'
+    filter_horizontal = ('tags',) # for ease of use
+
+admin.site.register(blog.Article, ArticleAdmin)
+admin.site.register(blog.Category)
+admin.site.register(blog.Tag)
