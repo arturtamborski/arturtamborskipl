6718cb6175d4f2fc7389dfbdcd77525bf27e57d3
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-14 01:10:22 +0100

    update home view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render
+from django.utils import timezone
+from . import models as blog
 
 def home(request):
-    return render(request, 'blog/home.html')
+    NUM_LAST_ARTICLES = 3
+
+    try:
+        # find three latest articles and sort them from latest one 
+        articles = blog.Article.objects.filter(date__lte=timezone.now()).order_by('-date')[:NUM_LAST_ARTICLES]
+    except ObjectDoesNotExist:
+        raise Http404('Article does not exist!')
+
+    return render(request, 'blog/home.html', { 'articles': articles })
