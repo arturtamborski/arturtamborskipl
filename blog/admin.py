44e2ed832117245237136f8d768e5336a0e6355c
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-15 13:59:48 +0100

    small changes in admin panel for models

--- blog/admin.py
+++ blog/admin.py
@@ -3,23 +3,21 @@ from . import models as blog
 
 class TagAdmin(admin.ModelAdmin):
     ordering = ('name',)
-    list_filter = ('id', 'name',)
     list_display = ('id', 'name',)
     search_fields = ('id', 'name',)
 
 class CategoryAdmin(admin.ModelAdmin):
     ordering = ('name',)
-    list_filter = ('id', 'name',)
     list_display = ('id', 'name',)
     search_fields = ('id', 'name',)
 
 class ArticleAdmin(admin.ModelAdmin):
     ordering = ('-date',)
-    list_filter = ('id', 'date',)
+    list_filter = ('date',)
     list_display = ('id', 'title', 'date', 'category',)
     search_fields = ('id', 'title', 'date', 'category',)
     date_hierarchy = 'date'
-    filter_horizontal = ('tags',) # for ease of use
+    filter_horizontal = ('tags',)
 
 admin.site.register(blog.Article, ArticleAdmin)
 admin.site.register(blog.Category, CategoryAdmin)
