17684652d7700fcdad96863f6a0c3d0f5a06d54b
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-16 14:14:39 +0100

    add url patterns for categories subapp

--- arturtamborskipl/urls.py
+++ arturtamborskipl/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    url(r'^', include('blog.urls')),
-    url(r'^blog', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='blog/', permanent=False)),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
-
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    #url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
