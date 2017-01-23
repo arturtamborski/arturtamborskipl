ae23d424323acc63e975e7ea9b6e2867dac15864
Artur Tamborski <tamborskiartur@gmail.com>
2017-01-23 21:04:35 +0100

    I forgot about this one, sorry

---
+++ blog/markup.py
@@ -0,0 +1,19 @@
+from markdown import markdown
+from typogrify.filters import typogrify
+
+def markup(text):
+    extensions = [
+    #    'abbr',
+    #    'def_list',
+        'fenced_code',
+    #    'footnotes',
+    #    'tables',
+    #    'smart_strong',
+    #    'admonition',
+        'headerid',
+    #    'nl2br',
+    #    'sane_lists',
+    #    #'smarty',
+    #    'toc',
+    ]
+    return markdown(text, output_format='html5', extensions=extensions)
