954ab681d8fd3dbaa7cd3823e38dd2d8c6ef075a
Artur Tamborski <tamborskiartur@gmail.com>
2017-02-11 12:44:41 +0100

    I don't even know... Fck.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views as blog
 
 urlpatterns = [
-    url(r'^article/(?P<slug>[\w-]+)/$', blog.article, name='article'),
-    url(r'^article/$', blog.article, name='article'),
+    url(r'^(?P<slug>[\w-]+)/$', blog.article, name='article'),
+    url(r'^$', blog.article, name='article'),
 
     url(r'^category/(?P<slug>[\w-]+)/$', blog.category, name='category'),
     url(r'^category/$', blog.category, name='category'),
@@ -13,7 +14,7 @@ urlpatterns = [
 
     url(r'^search/$', blog.search, name='search'),
 
-    url(r'^about/$', blog.about, name='about'),
+    url(r'^about/$', TemplateView.as_view(template_name='blog/about.html'), name='about'),
 
-    url(r'^$', blog.home, name='home'),
+    url(r'^$', blog.article, name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,74 +1,50 @@
 from django.utils import timezone
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from . import models as blog
 
-PAGE_MAX = 5
 
-
-def get_objects(request, objects, slug=None):
+def article(request, slug=None):
+    objects = blog.Article.objects.published()
     if slug is not None:
         objects = objects.filter(slug=slug)
 
-    page = request.GET.get('p', 1)
-    paginator = Paginator(objects, PAGE_MAX)
-
-    try:
-        objects = paginator.page(page)
-    except PageNotAnInteger:
-        objects = paginator.page(1)
-    except EmptyPage:
-        objects = paginator.page(paginator.num_pages)
-
-    return objects
-
-def article(request, slug=None):
-    articles = get_objects(
-        request, blog.Article.objects.published(), slug)
-
     return render(request, 'blog/article.html', {
         'isroot': slug is None,
-        'articles': articles,
+        'articles': objects,
         })
 
 
 
 def category(request, slug=None):
-    categories = get_objects(
-            request, blog.Category.objects.all(), slug)
+    objects = blog.Category.objects.all()
+    if slug is not None:
+        objects = objects.filter(slug=slug)
 
     return render(request, 'blog/category.html', {
         'isroot': slug is None,
-        'categories': categories,
+        'categories': objects,
         })
 
 
 
 def tag(request, slug=None):
-    tags = get_objects(
-            request, blog.Tag.objects.all(), slug)
+    objects = blog.Tag.objects.all()
+    if slug is not None:
+        objects = objects.filter(slug=slug)
 
     return render(request, 'blog/tag.html', {
         'isroot': slug is None,
-        'tags': tags,
+        'tags': objects,
         })
 
 
 
 def search(request):
     q = request.GET.get('q', '')
-    objects = get_objects(
-            request, blog.Article.objects.search(q), slug)
+    objects = blog.Article.objects.search(q)
 
     return render(request, 'blog/search.html', {
             'objects': objects,
         })
 
-
-
-def home(request):
-    return article(request)
-
-def about(request):
-    return article(request, slug='about')
