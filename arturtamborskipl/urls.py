bfd31e3950812fbb1ff7e81990d79e2b2b40a3a1
Artur Tamborski <tamborskiartur@gmail.com>
2017-02-26 10:49:29 +0100

    add sitemap.xml

--- arturtamborskipl/urls.py
+++ arturtamborskipl/urls.py
@@ -4,9 +4,13 @@ from django.views.generic import RedirectView
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 
+from blog.sitemaps import BlogSitemap
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    #url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'blog': BlogSitemap}}),
+
     url(r'^', include('blog.urls')),
 ]
