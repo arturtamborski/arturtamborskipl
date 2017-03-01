b89e7288bf11e20ca6b0775d8a4d427e7f1b912b
Artur Tamborski <tamborskiartur@gmail.com>
2017-03-01 16:38:29 +0100

    use {% url %} in anchors, add trailing slashes in urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,18 +5,18 @@ from . import feeds
 from . import views
 
 urlpatterns = [
-    url(r'^post/(?P<slug>[\w-]+)$', views.article, name='article'),
+    url(r'^post/(?P<slug>[\w-]+)/$', views.article, name='article'),
 
-    url(r'^category/(?P<slug>[\w-]+)/feed$', feeds.CategoryRssFeed(), name='category-rss-feed'),
-    url(r'^category/(?P<slug>[\w-]+)/rss$', feeds.CategoryRssFeed(), name='category-rss-feed'),
-    url(r'^category/(?P<slug>[\w-]+)/atom$', feeds.CategoryAtomFeed(), name='category-atom-feed'),
-    url(r'^category/(?P<slug>[\w-]+)$', views.category, name='category'),
+    url(r'^category/(?P<slug>[\w-]+)/feed/$', feeds.CategoryRssFeed(), name='category-rss-feed'),
+    url(r'^category/(?P<slug>[\w-]+)/rss/$', feeds.CategoryRssFeed(), name='category-rss-feed'),
+    url(r'^category/(?P<slug>[\w-]+)/atom/$', feeds.CategoryAtomFeed(), name='category-atom-feed'),
+    url(r'^category/(?P<slug>[\w-]+)/$', views.category, name='category'),
     url(r'^category/$', views.category, name='category'),
 
-    url(r'^tag/(?P<slug>[\w-]+)/feed$', feeds.TagRssFeed(), name='tag-rss-feed'),
-    url(r'^tag/(?P<slug>[\w-]+)/rss$', feeds.TagRssFeed(), name='tag-rss-feed'),
-    url(r'^tag/(?P<slug>[\w-]+)/atom$', feeds.TagAtomFeed(), name='tag-atom-feed'),
-    url(r'^tag/(?P<slug>[\w-]+)$', views.tag, name='tag'),
+    url(r'^tag/(?P<slug>[\w-]+)/feed/$', feeds.TagRssFeed(), name='tag-rss-feed'),
+    url(r'^tag/(?P<slug>[\w-]+)/rss/$', feeds.TagRssFeed(), name='tag-rss-feed'),
+    url(r'^tag/(?P<slug>[\w-]+)/atom/$', feeds.TagAtomFeed(), name='tag-atom-feed'),
+    url(r'^tag/(?P<slug>[\w-]+)/$', views.tag, name='tag'),
     url(r'^tag/$', views.tag, name='tag'),
 
     url(r'^search/$', views.search, name='search'),
