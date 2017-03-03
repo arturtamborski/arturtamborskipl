80371019a8d64673d9baa6fb2a293edc0556acbb
Artur Tamborski <tamborskiartur@gmail.com>
2017-03-04 00:00:08 +0100

    commit forgoten files

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^search/$', views.search, name='search'),
 
     url(r'^about/$', TemplateView.as_view(template_name='blog/about.html'), name='about'),
-
+    url(r'^projects/$', TemplateView.as_view(template_name='blog/projects.html'), name='projects'),
 
     url(r'^$', views.article, name='home'),
 ]
