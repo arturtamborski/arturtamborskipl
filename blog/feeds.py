b4be2a9a003e90a0573eec3790750cdb10f14658
Artur Tamborski <tamborskiartur@gmail.com>
2017-03-06 20:56:42 +0100

    fix articles feed and <description> section

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,55 +1,57 @@
 from django.contrib.syndication.views import Feed
-from django.utils.feedgenerator import Atom1Feed
 from django.urls import reverse
+
 from . import models
 
+FEED_DESCRIPTION_MAX = 200
 
 class ArticleFeed(Feed):
     title = 'Latest posts'
+    link  = ''
 
-    def get_object(self, request, slug):
-        return models.Articles.objects.published().get(slug=slug)
-
-    def title(self, item):
-        return item.title
-
-    def description(self, item):
-        return (item.content[:80] + '...') if len(item.content) > 80 else item.content
-
-    def link(self, item):
-        return reverse('article', args=[item.slug])
-
-    def items(self, item):
+    def items(self):
         return models.Article.objects.published()
+
+    def item_description(self, item):
+        if len(item.content) > FEED_DESCRIPTION_MAX:
+            return item.content_html[:FEED_DESCRIPTION_MAX] + '...'
+        else:
+            return item.content_html
 
 
 class CategoryFeed(Feed):
-    description = ''
+    link = ''
+
+    def title(self, item):
+        return 'Latest posts for category: ' + item.name
 
     def get_object(self, request, slug):
         return models.Category.objects.get(slug=slug)
 
-    def title(self, item):
-        return item.name
-
-    def link(self, item):
-        return item.get_absolute_url()
-
     def items(self, item):
         return models.Article.objects.published().filter(category=item)
 
+    def item_description(self, item):
+        if len(item.content) > FEED_DESCRIPTION_MAX:
+            return item.content_html[:FEED_DESCRIPTION_MAX] + '...'
+        else:
+            return item.content_html
+
 
 class TagFeed(Feed):
-    description = ''
+    link = ''
+
+    def title(self, item):
+        return item.name
 
     def get_object(self, request, slug):
         return models.Tag.objects.get(slug=slug)
 
-    def title(self, item):
-        return item.name
-
-    def link(self, item):
-        return item.get_absolute_url()
-
     def items(self, item):
         return models.Article.objects.published().filter(tags=item)
+
+    def item_description(self, item):
+        if len(item.content) > FEED_DESCRIPTION_MAX:
+            return item.content_html[:FEED_DESCRIPTION_MAX] + '...'
+        else:
+            return item.content_html
